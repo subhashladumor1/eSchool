dc68898a653ebc4364bb848ce586239bbba52c85
Subhash Ladumor <79623012+subhashladumor1@users.noreply.github.com>
2022-03-03 12:20:05 +0530

    11th Completed
    11 VIDEO admin Dashboard = Add Course, Edit Course, SESSION with LOGIN

--- Admin/adminDashboard.php
+++ Admin/adminDashboard.php
@@ -1,13 +1,79 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
-</head>
-<body>
-    <h1>Welcome to Admin Dashboard</h1>
-    <a href="../logout.php">Logout Button </a>
-</body>
-</html>
+<?php
+include('./admininclude/header.php');
+?>
+
+<div class="col-sm-9 mt-5">
+    <div class="row mx-5 text-center">
+        <div class="col-sm-4 mt-5">
+            <div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
+                <div class="card-header">Courses</div>
+                <div class="card-body">
+                    <h4 class="card-title">
+                        5
+                    </h4>
+                    <a class="btn text-white" href="#">View</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4 mt-5">
+            <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                <div class="card-header">students</div>
+                <div class="card-body">
+                    <h4 class="card-title">
+                        25
+                    </h4>
+                    <a class="btn text-white" href="#">View</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4 mt-5">
+            <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+                <div class="card-header">Sold</div>
+                <div class="card-body">
+                    <h4 class="card-title">
+                        3
+                    </h4>
+                    <a class="btn text-white" href="#">View</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="mx-5 mt-5 text-center">
+        <!--Table -->
+        <p class="bg-dark text-white p-2">course ordered</p>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Order ID</th>
+                    <th scope="col">Course ID</th>
+                    <th scope="col">Student Email</th>
+                    <th scope="col">Order Date</th>
+                    <th scope="col">Amount</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">22</th>
+                    <td>100</td>
+                    <td>sonam@gmail.com</td>
+                    <td>20/10/2020</td>
+                    <td>2000</td>
+                    <td><button type="submit" class="btn
+                                                    btn-secondary" name="delete" value="Delete"><i class="far fa-trash-alt"></i></buttom>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+</div>
+</div>
+
+</div>
+<!--div row close from header -->
+</div>
+<!--div container-fluid close from header -->
+<?php
+    include('./admininclude/footer.php');
+?>
